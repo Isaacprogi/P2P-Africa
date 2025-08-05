83434bcb2064d4c19a2a29aeb7c5c92c8fcec181
isaacprogi <isaaconyes80@gmail.com>
2025-08-05 12:27:39 +0100

    format name

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="min-h-screen bg-gray-50">
     <Navbar />
-    <HeroSection />
-    <EventsSection />
-    <FooterSection />
+    <Hero />
+    <Events />
+    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue'
-import HeroSection from './components/Hero.vue'
-import EventsSection from './components/Events.vue'
-import FooterSection from './components/Footer.vue'
+import Hero from './components/Hero.vue'
+import Events from './components/Events.vue'
+import Footer from './components/Footer.vue'
 </script>
